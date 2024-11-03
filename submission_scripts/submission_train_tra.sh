1e6a942c24ef2ee25efe182c6d2fba8da89f5a7d
Eleonora Cannavacciuolo <Eleonora.Cannavacciuolo@unige.ch>
2024-11-03 11:50:19 +0100

    add model and submission script

---
+++ submission_scripts/submission_train_tra.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+#SBATCH --partition=shared-gpu
+#SBATCH --time=06:00:00
+#SBATCH --gpus=1
+#SBATCH --output=kraken-%j.out
+#SBATCH --mem=24GB
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1,VramPerGpu:24GB
+
+module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
+source ~/kraken-env/bin/activate
+
+OUTPUT_NAME="/home/users/c/cannavac/BnF_fr19152/models/trained_transcription_model"
+XML_FOLDER="/home/users/c/cannavac/BnF_fr19152/data_gold"
+BASE_MODEL="/home/users/c/cannavac/BnF_fr19152/models/CATMUS_Medieval.mlmodel"
+
+echo "KETOS training"
+srun ketos train -o $OUTPUT_NAME \
+	-f alto -d cuda:0 --resize union \
+	--epochs 50 --schedule cosine \
+	-B 8 -r 0.003 \
+	-i $BASE_MODEL \
+	-t "${XML_FOLDER}/*.xml"
